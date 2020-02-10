70770e5acd73cb8b75bc0af49e704fc96c594c79
Tiffany McLean <tamclean324@gmail.com>
2020-02-09 23:45:03 -0500

    completes assignment

--- list2.py
+++ list2.py
@@ -23,8 +23,7 @@ Kenzie assignment: List2
 
 
 def remove_adjacent(nums):
-    # your code here
-    return
+    return [elem for i, elem in enumerate(nums) if i == 0 or nums[i-1] != elem]
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -33,8 +32,12 @@ def remove_adjacent(nums):
 # Hint: Don't use `sort` or `sorted` -- they are not O(n) linear time, and the two lists
 # are already provided in ascending sorted order.
 def linear_merge(list1, list2):
-    # your code here
-    return
+    result = []
+
+    while list1 and list2:
+        result.append((list1 if list1[-1] > list2[-1] else list2).pop(-1))
+
+    return (result + list1 + list2)[-1::-1]
 
 
 # Simple provided test() function used in main() to print
